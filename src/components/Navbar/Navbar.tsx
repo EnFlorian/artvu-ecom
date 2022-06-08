139beb8289514cfa46f3f324daf02030acbcecaa
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-06-08 11:58:29 +0200

    Added not found page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__wrapper container">
-        <h1 className="navbar__logo">Artvue</h1>
+        <h1 className="navbar__logo">
+          <Link to="/">Artvu</Link>
+        </h1>
         <ul className="navbar__navlinks">
           <li className="navbar__navlink">
             <Link to="/">Home</Link>
@@ -18,7 +21,10 @@ const Navbar = () => {
           </li>
         </ul>
         <ul className="navbar__navlinks">
-          <li className="navbar__navlink">Cart</li>
+          <li className="navbar__navlink">
+            Cart
+            <FaShoppingCart className="navbar__shopping-cart" />
+          </li>
           <li className="navbar__navlink">Login</li>
         </ul>
       </div>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,9 @@
+import "./NotFound.scss";
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <p>Page not found</p>
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__subtitle">Page not found</p>
     </div>
   );
 };
