0b04b640ab37803fbade78d5a3cd7766f768b3ec
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-06-08 21:02:53 +0200

    Fixed conditional rendering bug

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./Auth.scss";
 const Auth = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   return (
     <section className="auth">
       <div className="container auth__wrapper">
         <div className="auth__form-container">
-          <h1 className="auth__title">Login</h1>
+          {isLogin ? <h1 className="auth__title">Login</h1> : <h1 className="auth__title">Register</h1>}
           <form className="auth__form">
             <input aria-label="Name" className="auth__input" type="text" placeholder="Please enter your name" />
             <input aria-label="Email" className="auth__input" type="email" placeholder="Please enter your email" />
@@ -14,13 +20,20 @@ const Auth = () => {
               type="password"
               placeholder="Pleade enter a password"
             />
-            <h2 className="auth__error">Error</h2>
-
-            <button className="auth__btn">Login</button>
+            {error && <h2 className="auth__error">{error}</h2>}
+            <button className="auth__btn">{isLogin ? "Login" : "Register"}</button>
           </form>
-          <p className="auth__alt">
-            <span className="auth__alt-link">Register</span> instead
-          </p>
+
+          {isLogin ? (
+            <p className="auth__alt">
+              <span className="auth__alt-link">Register</span> instead
+            </p>
+          ) : (
+            <p className="auth__alt">
+              {" "}
+              <span className="auth__alt-link">Login</span> instead
+            </p>
+          )}
         </div>
       </div>
     </section>
