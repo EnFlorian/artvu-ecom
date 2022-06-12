b13dc15f91252e0a8a3f115714fd1f7b100d4a97
unknown <33962956+EnFlorian@users.noreply.github.com>
2022-06-12 13:46:12 +0200

    Added styling for product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,47 +1,53 @@
 import { Link } from "react-router-dom";
 import "./Product.scss";
+import image from "../assets/images/products/3.jpg";
 
 const Product = () => {
   return (
     <article className="product">
-      <div className="product__wrapper">
+      <div className="product__wrapper container">
         <Link className="product__back-link" to="/products">
           Back to products
         </Link>
-        <section className="product__left-content">
-          <img className="product__image" src="https://via.placeholder.com/300x300" alt="" />
-          <div className="product__more-images">
-            <img className="product__image" src="https://via.placeholder.com/300x300" alt="" />
-            <img className="product__image" src="https://via.placeholder.com/300x300" alt="" />
-            <img className="product__image" src="https://via.placeholder.com/300x300" alt="" />
-          </div>
-        </section>
-        <section className="product__right-content">
-          <h1 className="product__title">Product Title</h1>
-          <p className="product__price">$100</p>
-          <p className="product__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nisl eu nisl porta, euismod egestas
-            nisl euismod.
-          </p>
-          <div className="product__details">
-            <div className="product__details-item">
-              <p className="product__details-item-title">Stock</p>
-              <p className="product__details-item-value">10</p>
+        <div className="product__content">
+          <section className="product__left-content">
+            <img className="product__image" src={image} alt="" />
+            <div className="product__more-images">
+              <img className="product__image-small" src={image} alt="" />
+              <img className="product__image-small" src={image} alt="" />
+              <img className="product__image-small" src={image} alt="" />
+              <img className="product__image-small" src={image} alt="" />
+              <img className="product__image-small" src={image} alt="" />
             </div>
-            <div className="product__details-item">
-              <p className="product__details-item-title">Artist</p>
-              <p className="product__details-item-value">S</p>
+          </section>
+          <section className="product__right-content">
+            <h1 className="product__title">Product Title</h1>
+            <p className="product__price">$100</p>
+            <p className="product__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nisl eu nisl porta, euismod egestas
+              nisl euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nisl eu nisl porta,
+              euismod egestas nisl euismod.
+            </p>
+            <div className="product__details">
+              <div className="product__details-item">
+                <p className="product__details-item-title">Stock:</p>
+                <p className="product__details-item-value">10</p>
+              </div>
+              <div className="product__details-item">
+                <p className="product__details-item-title">Artist:</p>
+                <p className="product__details-item-value">Maximilian Fabour</p>
+              </div>
+              <div className="product__details-item">
+                <p className="product__details-item-title">Category:</p>
+                <p className="product__details-item-value">Modern Painting</p>
+              </div>
+              <div className="product__details-item">
+                <p className="product__details-item-title">Online Ordering:</p>
+                <p className="product__details-item-value">Available</p>
+              </div>
             </div>
-            <div className="product__details-item">
-              <p className="product__details-item-title">Category</p>
-              <p className="product__details-item-value">S</p>
-            </div>
-            <div className="product__details-item">
-              <p className="product__details-item-title">Online Ordering</p>
-              <p className="product__details-item-value">S</p>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     </article>
   );
