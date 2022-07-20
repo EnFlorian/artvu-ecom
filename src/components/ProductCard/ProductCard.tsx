959bf8026765579580ad3b71348fb793cc1d3ee2
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-07-20 22:32:29 +0200

    Fix: Added link to images

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import { IProduct } from "../../types/state";
 import "./ProductCard.scss";
 
-const ProductCard = ({ name, price, image }: IProduct) => {
+const ProductCard = ({ id, name, price, image }: IProduct) => {
   return (
     <div className="product-card">
-      <img className="product-card__image" src={image} alt="" />
-      <div className="product-card__info">
-        <h3 className="product-card__name">{name}</h3>
-        <p className="product-card__price">{`$${price}`}</p>
-      </div>
+      <Link to={`/products/${id}`}>
+        <img className="product-card__image" src={image} alt="" />
+        <div className="product-card__info">
+          <h3 className="product-card__name">{name}</h3>
+          <p className="product-card__price">{`$${price}`}</p>
+        </div>
+      </Link>
     </div>
   );
 };
